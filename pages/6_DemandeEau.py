d6253c967ff357f8f3caf3c16a495166e9e5f3ea
Rémi Verny <remi.verny@student-cs.fr>
2026-04-14 18:41:34 +0200

    Rearrangement of the chart and parameters and translation of page 6

--- pages/6_DemandeEau.py
+++ pages/6_DemandeEau.py
@@ -5,20 +5,12 @@ from utils.init_session import init_session_defaults
 
 init_session_defaults()
 show_menu ()
-st.title("💧  Demande en eau")
+st.title("💧  Water demand")
 
-MOIS = ["Jan", "Fév", "Mar", "Avr", "Mai", "Jun", "Jul", "Aoû", "Sep", "Oct", "Nov", "Déc"]
+MOIS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 with st.form("form_demande_eau"):
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        crop_area = st.number_input("Surface cultivée (ha)", value=st.session_state.crop_area, step=0.1, format="%.1f")
-    with col2:
-        percolation_rate = st.number_input("Taux percolation riz (mm/j)", value=st.session_state.percolation_rate, step=0.5, format="%.1f")
-    with col3:
-        conveyance_eff = st.number_input("Efficacité convoyage (-)", value=st.session_state.conveyance_eff, step=0.01, min_value=0.0, max_value=1.0, format="%.2f")
-
-    st.markdown("#### Demande mensuelle brute (m³/mois par hax10)")
+    st.markdown("#### Monthly raw demand in water (m³/month)")
 
     gw = st.session_state.gw_demand
     cols = st.columns(6)
@@ -28,16 +20,25 @@ with st.form("form_demande_eau"):
             new_gw[i] = st.number_input(MOIS[i], value=float(gw[i]), step=0.01, format="%.5f", key=f"gw_{i}")
     gw_demand = new_gw
 
-    chart_df = pd.DataFrame({"Mois": MOIS, "Demande (m³)": new_gw}).set_index("Mois")
+    chart_df = pd.DataFrame({"Mois": MOIS, "Demande (m³)": new_gw})
+    chart_df["Mois"] = pd.Categorical(chart_df["Mois"], categories=MOIS, ordered=True)
+    chart_df = chart_df.set_index("Mois")
     st.bar_chart(chart_df, height=220, color="#2563eb")
+    
+    submitted = st.form_submit_button("Confirm")
 
-    submitted = st.form_submit_button("Valider")
+st.write("""
+### Help
+If you do not know your monthly needs for water, you may enter the total area of land you want to irrigate in the box below 
+""")
+crop_area = st.number_input("Area with cultures (ha)", value=st.session_state.crop_area, step=0.1, format="%.1f")
+sub = st.button("Confirm")
 
-
-if submitted:
+if submitted or sub:
     st.session_state.crop_area = crop_area
-    st.session_state.percolation_rate = percolation_rate
-    st.session_state.conveyance_eff = conveyance_eff
     st.session_state.gw_demand = new_gw
     
-    st.success("Configuration sauvegardée !")
+    st.success("Datas saved successfully")
+
+if st.button("Next", type='primary'):
+    st.switch_page('pages/7_Hydrogeologie.py')
